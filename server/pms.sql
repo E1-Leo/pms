a94c4a8e0a5457b74edf71ded8386b110d05825f
unknown <1294260255@qq.com>
2019-05-12 20:11:51 +0800

    pms

--- server/pms.sql
+++ server/pms.sql
@@ -10,90 +10,95 @@ Target Server Type    : MYSQL
 Target Server Version : 80015
 File Encoding         : 65001
 
-Date: 2019-05-11 13:53:12
+Date: 2019-05-12 20:11:43
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for ann
+-- Table structure for announcement
 -- ----------------------------
-DROP TABLE IF EXISTS `ann`;
-CREATE TABLE `ann` (
-  `id` int(11) NOT NULL COMMENT '公告id',
-  `anntitle` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '公告标题',
-  `anninfo` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '公告内容',
-  `time` date DEFAULT NULL COMMENT '发布时间',
-  `username` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '发布人',
+DROP TABLE IF EXISTS `announcement`;
+CREATE TABLE `announcement` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '公告id',
+  `announcementtitle` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '公告标题',
+  `announcementinfo` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '公告内容',
+  `publishtime` bigint(20) DEFAULT NULL COMMENT '发布时间',
+  `publishname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '发布人',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of ann
+-- Records of announcement
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for divi
+-- Table structure for department
 -- ----------------------------
-DROP TABLE IF EXISTS `divi`;
-CREATE TABLE `divi` (
-  `id` int(11) NOT NULL COMMENT '部门id',
-  `department` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '部门',
-  `depinfo` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '部门信息',
+DROP TABLE IF EXISTS `department`;
+CREATE TABLE `department` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '部门id',
+  `department` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '部门',
+  `departmentinfo` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '部门信息',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of divi
+-- Records of department
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for posi
+-- Table structure for job
 -- ----------------------------
-DROP TABLE IF EXISTS `posi`;
-CREATE TABLE `posi` (
-  `id` int(11) NOT NULL COMMENT '职位id',
-  `jobname` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '职位',
+DROP TABLE IF EXISTS `job`;
+CREATE TABLE `job` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '职位id',
+  `jobname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '职位',
   `jobinfo` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '职位信息',
   `banci` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '班次',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of posi
+-- Records of job
 -- ----------------------------
+INSERT INTO `job` VALUES ('1', '总经理', '总经理', '');
 
 -- ----------------------------
 -- Table structure for staff
 -- ----------------------------
 DROP TABLE IF EXISTS `staff`;
 CREATE TABLE `staff` (
-  `id` int(11) NOT NULL COMMENT '员工id',
-  `name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '姓名',
-  `department` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '部门',
-  `jobname` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '职位',
-  `jobtime` date DEFAULT NULL COMMENT '入职时间',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '员工id',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '姓名',
+  `department` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '部门',
+  `jobname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '职位',
+  `jobtime` bigint(20) DEFAULT NULL COMMENT '入职时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of staff
 -- ----------------------------
+INSERT INTO `staff` VALUES ('1', 'Jane', '运营部', '总监', '1557417600000');
+INSERT INTO `staff` VALUES ('2', '吴霞', '销售部', '经理', '1558886400000');
+INSERT INTO `staff` VALUES ('3', 'Wuyifan', '财务部', '会计', '1557504000000');
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL COMMENT '用户id',
-  `username` varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户名',
-  `password` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '密码',
-  `repassword` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '确认密码',
-  `department` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '部门',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户id',
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户名',
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '密码',
+  `department` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '部门',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', 'admin', '123456', '123456', 'a');
+INSERT INTO `user` VALUES ('1', 'admin', '$2b$10$WcezL46MuWkUD40KJO2wouNWbwCGuYQ95qrzdWpL4hcHY8eB0vpp2', '技术部');
+INSERT INTO `user` VALUES ('2', 'leopoe', '$2b$10$weI8d/7ibBwwjOKWNg4j0.C2gv8NtLcQ65C0kuo4EKCWUKQ6qzGfK', '行政部');
+INSERT INTO `user` VALUES ('7', 'dd', null, '运营部');
